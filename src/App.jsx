71d85afa071c2ca196006c3f5ef87f6efbe1d6f8
Jahir Medina <jahirtunmedina@gmail.com>
2025-04-28 07:06:28 -0600

    Implementación de la marquesina

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Marquesina from "./components/Marquesina.jsx"
 import NavBar from "./components/NavBar.jsx"
 import Hero from "./sections/Hero.jsx"
 import ShowcaseSection from "./sections/ShowcaseSection.jsx"
@@ -8,6 +9,7 @@ const app = () => {
     <NavBar/>
     <Hero />
     <ShowcaseSection />
+    <Marquesina />
     </>
   )
 }
