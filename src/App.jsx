a1ca95bcf5b89d4ff83c795eda3efe8ae7cf3c40
Jahir Medina <jahirtunmedina@gmail.com>
2025-04-28 23:25:54 -0600

    Implementación de la sección Experiencia con animaciones y scroll

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import NavBar from "./components/NavBar.jsx"
 import FeatureCards from "./sections/FeatureCards.jsx"
 import Hero from "./sections/Hero.jsx"
 import ShowcaseSection from "./sections/ShowcaseSection.jsx"
+import Experiencia from "./sections/Experiencia.jsx"
 
 const app = () => {
   return (
@@ -12,6 +13,7 @@ const app = () => {
     <ShowcaseSection />
     <Marquesina />
     <FeatureCards />
+    <Experiencia />
     </>
   )
 }
